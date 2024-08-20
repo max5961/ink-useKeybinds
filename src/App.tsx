1d964f7c8a6361052ef06df753bf8f413b38ae14
Maxwell Morrissey <mmorrissey5961@gmail.com>
2024-08-19 23:57:25 -0400

    Restructure useKeyBinds hook from quild project

---
+++ src/App.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { Box, Text } from "ink";
+import { useKeybinds, KbConfig } from "./useKeybinds.js";
+
+const kbs = {
+    // 'f' input triggers 'foo' command
+    foo: { input: "f" },
+
+    // Multiple inputs can trigger the same command
+    bar: [{ input: "b" }, { input: "a" }, { input: "r" }],
+
+    // Input can be up to 2 chars long
+    quz: { input: "qq" },
+
+    // Uppercase input is separate from lowercase
+    FOO: [{ input: "F" }, { input: "O" }],
+
+    // Uppercase and lowercase input can be mixed
+    BAR: [{ input: "Ba" }, { input: "Br" }],
+    QUZ: [{ input: "qQ" }, { input: "Qq" }],
+
+    ctrl_foobar: [
+        // Non alphanumeric keys can be mixed with lowercase chars
+        { input: "fb", key: "ctrl" },
+
+        // Non alphanumeric keys CANNOT be mixed with uppercase chars
+        { input: "FB", key: "ctrl" },
+    ],
+
+    // Standalone non alphanumeric keys can trigger commands
+    just_return: { key: "return" },
+} satisfies KbConfig;
+
+export default function App(): React.ReactNode {
+    const [msg, setMsg] = useState<string>("No commands yet!");
+
+    const { command, register } = useKeybinds(
+        (cmd) => {
+            if (cmd === "just_return") {
+                setMsg("just_return triggered");
+            }
+
+            if (cmd === "ctrl_foobar") {
+                setMsg("ctrl_foobar triggered");
+            }
+
+            if (cmd === "foo") {
+                setMsg("foo triggered");
+            }
+
+            if (cmd === "bar") {
+                setMsg("bar triggered");
+            }
+
+            if (cmd === "quz") {
+                setMsg("quz triggered");
+            }
+
+            if (cmd === "FOO") {
+                setMsg("FOO triggered");
+            }
+
+            if (cmd === "BAR") {
+                setMsg("BAR triggered");
+            }
+
+            if (cmd === "QUZ") {
+                setMsg("QUZ triggered");
+            }
+        },
+        kbs,
+        { trackState: true },
+    );
+
+    return (
+        <>
+            <Text>
+                Current command is:
+                <Text color="green">{` ${msg}!`}</Text>
+            </Text>
+            <Box display="flex" flexDirection="column">
+                <Text>{`Command: ${command ? command : ""}`}</Text>
+                <Text>{`Register: ${register}`}</Text>
+            </Box>
+        </>
+    );
+}
---
+++ src/Keybinds.ts
@@ -0,0 +1,92 @@
+import { Key } from "ink";
+import { KbConfig, Command, Binding } from "./useKeybinds.js";
+
+export class KeyBinds<T extends KbConfig = KbConfig> {
+    private config: T;
+    private register: string;
+    private command: Command<T> | null;
+    private keyRegister: string;
+
+    constructor(config: T) {
+        this.register = "";
+        this.keyRegister = "";
+        this.command = null;
+        this.config = config;
+    }
+
+    private clearCommand(): void {
+        this.command = null;
+    }
+
+    private clearRegister(): void {
+        this.register = "";
+    }
+
+    private pushRegister(char: string): void {
+        if (this.register.length >= 2) {
+            this.clearRegister();
+        }
+
+        this.register += char;
+    }
+
+    private setCommand(command: Command<T>): void {
+        this.clearRegister();
+        this.command = command;
+    }
+
+    getCommand(): Command<T> | null {
+        return this.command;
+    }
+
+    getRegister(): string {
+        return this.register;
+    }
+
+    getKeyRegister(): string {
+        return this.keyRegister;
+    }
+
+    handleStdIn(input: string, key: Key): void {
+        this.clearCommand();
+        this.pushRegister(input);
+
+        for (const objKey in this.config) {
+            const binding = this.config[objKey];
+
+            if (Array.isArray(binding)) {
+                for (const b of binding) {
+                    const match = this.checkMatch(b, key);
+                    if (match) return this.setCommand(objKey as Command<T>);
+                }
+            } else {
+                const match = this.checkMatch(binding as Binding, key);
+                if (match) return this.setCommand(objKey as Command<T>);
+            }
+        }
+    }
+
+    private checkMatch(binding: Binding, key: Key): boolean {
+        // Prevent ctrl + f triggering 'f'
+        if (key.ctrl) {
+            // Special key + char input
+            if (binding.key && binding.input) {
+                return key[binding.key] && this.register === binding.input;
+            }
+
+            // Special key only
+            if (binding.key && !binding.input) {
+                return key[binding.key];
+            }
+
+            return false;
+        }
+
+        // Char input only
+        if (!binding.key && binding.input) {
+            return this.register === binding.input;
+        }
+
+        return false;
+    }
+}
